7cc25e252631141a0b6a307c7953105cd309487f
SumanSingha05 <sumansingha8654@gmail.com>
2025-02-17 22:49:56 +0530

    cookie parser added

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv"
 import connectDB from "./config/database.js";
 import userRoute from "./routes/userRoute.js"
+import cookieParser from "cookie-parser"
 dotenv.config({});
 
 const app = express();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 //middleware
 app.use(express.json());
+app.use(cookieParser());
 
 // routes
 app.use("/api/v1/user",userRoute);
