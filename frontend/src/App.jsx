7e1af9350b36f135aca4bb6a25db061538d72d2c
SumanSingha05 <sumansingha8654@gmail.com>
2025-02-18 22:34:15 +0530

    tailwind and daisy ui initialized

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import React from 'react'
 
-import './App.css'
 
 function App() {
   return (
    <div className='App'>
     Lets build chat app
+    <button className="btn btn-secondary">Secondary</button>
+    <button className="btn btn-active btn-secondary">Secondary</button>
+    <button className="btn btn-primary">Primary</button>
    </div>
   );
 }
