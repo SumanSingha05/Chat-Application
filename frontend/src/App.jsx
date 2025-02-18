efa0ee109559c2cb791fa3f68af646b400beada4
SumanSingha05 <sumansingha8654@gmail.com>
2025-02-18 23:22:32 +0530

    routes of frontend site initialized home, login, register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,29 @@
-import { useState } from 'react'
 import React from 'react'
+import './App.css';
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import HomePage from './components/HomePage';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<HomePage/>
+  },
+  {
+    path:"/register",
+    element:<Signup/>
+  },
+  {
+    path:"/login",
+    element:<Login/>
+  }
+])
 
 function App() {
   return (
    <div className='App'>
-    Lets build chat app
-    <button className="btn btn-secondary">Secondary</button>
-    <button className="btn btn-active btn-secondary">Secondary</button>
-    <button className="btn btn-primary">Primary</button>
+    <RouterProvider router={router}/>
    </div>
   );
 }
