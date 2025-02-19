2a342215137aefb439cb87ec329856c4a5051984
SumanSingha05 <sumansingha8654@gmail.com>
2025-02-19 15:01:15 +0530

    signup page done

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,53 +1,60 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Signup = () => {
   return (
     <div className='min-w-96 mx-auto'>
-    <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100'>
-      <h1 className='text-3xl font-bold text-center'>Signup</h1>
-      <form action="">
-        <div>
-          <label className='label p-2'>
-            <span className='text-base label-text'>Full Name</span>
-          </label>
-          <input
-           className='w-full input input-bordered h-10'
-           type="text" placeholder='Full Name'></input>
-        </div>
-        <div>
-          <label className='label p-2'>
-            <span className='text-base label-text'>Username</span>
-          </label>
-          <input
-           className='w-full input input-bordered h-10'
-           type="text" placeholder='Username'></input>
-        </div><div>
-          <label className='label p-2'>
-            <span className='text-base label-text'>Password</span>
-          </label>
-          <input
-           className='w-full input input-bordered h-10'
-           type="password" placeholder='Suman'></input>
-        </div><div>
-          <label className='label p-2'>
-            <span className='text-base label-text'>Confirm Password</span>
-          </label>
-          <input
-           className='w-full input input-bordered h-10'
-           type="password" placeholder='Suman'></input>
-        </div>
-        <div className='flex items-center my-4'>
-        <div className='flex items-center'>
-          <p>Male</p>
-          <input type="checkbox" defaultChecked className="checkbox mx-2" />
-        </div>
-        <div className='flex items-center'>
-          <p>Female</p>
-          <input type="checkbox" defaultChecked className="checkbox mx-2" />
-        </div>
-        </div>
-      </form>
-    </div>
+      <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100'>
+        <h1 className='text-3xl font-bold text-center'>Signup</h1>
+        <form action="">
+          <div>
+            <label className='label p-2'>
+              <span className='text-base label-text'>Full Name</span>
+            </label>
+            <input
+              className='w-full input input-bordered h-10'
+              type="text" placeholder='Full Name'></input>
+          </div>
+          <div>
+            <label className='label p-2'>
+              <span className='text-base label-text'>Username</span>
+            </label>
+            <input
+              className='w-full input input-bordered h-10'
+              type="text" placeholder='Username'></input>
+          </div><div>
+            <label className='label p-2'>
+              <span className='text-base label-text'>Password</span>
+            </label>
+            <input
+              className='w-full input input-bordered h-10'
+              type="password" placeholder='Suman'></input>
+          </div><div>
+            <label className='label p-2'>
+              <span className='text-base label-text'>Confirm Password</span>
+            </label>
+            <input
+              className='w-full input input-bordered h-10'
+              type="password" placeholder='Suman'></input>
+          </div>
+          <div className='flex items-center my-4'>
+            <div className='flex items-center'>
+              <p>Male</p>
+              <input type="checkbox" defaultChecked className="checkbox mx-2" />
+            </div>
+            <div className='flex items-center'>
+              <p>Female</p>
+              <input type="checkbox" defaultChecked className="checkbox mx-2" />
+            </div>
+          </div>
+          <p className='text-center'> Already have an account? <Link to="/login">
+            signup
+          </Link></p>
+          <div>
+            <button className='btn btn-block btn-sm mt-2 border border-slate-700'>Signup</button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
