6fc699c9b39cf4060e9f406fd493f3059d59f89f
SumanSingha05 <sumansingha8654@gmail.com>
2025-02-19 18:12:11 +0530

    male female select feature added

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,35 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 
 const Signup = () => {
+  const [user, setUser] = useState({
+    fullName: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    gender: "",
+  });
+  const handleCheckbox = (gender) => {
+    setUser({ ...user, gender })
+  }
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    console.log(user);
+
+  }
   return (
     <div className='min-w-96 mx-auto'>
       <div className='w-full p-6 rounded-lg shadow-md bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100'>
         <h1 className='text-3xl font-bold text-center'>Signup</h1>
-        <form action="">
+        <form onSubmit={onSubmitHandler} action="">
           <div>
             <label className='label p-2'>
               <span className='text-base label-text'>Full Name</span>
             </label>
             <input
+              value={user.fullName}
+              onChange={(e) => setUser({ ...user, fullName: e.target.value })}
               className='w-full input input-bordered h-10'
-              type="text" 
-              placeholder='Full Name'/>
+              type="text"
+              placeholder='Full Name' />
           </div>
           <div>
             <label className='label p-2'>
               <span className='text-base label-text'>Username</span>
             </label>
             <input
+              value={user.username}
+              onChange={(e) => setUser({ ...user, username: e.target.value })}
+
               className='w-full input input-bordered h-10'
-              type="text" 
-              placeholder='Username'/>
+              type="text"
+              placeholder='Username' />
           </div><div>
             <label className='label p-2'>
               <span className='text-base label-text'>Password</span>
             </label>
             <input
+              value={user.password}
+              onChange={(e) => setUser({ ...user, password: e.target.value })}
+
               className='w-full input input-bordered h-10'
               type="password"
               placeholder='Password' />
@@ -38,25 +61,38 @@ const Signup = () => {
               <span className='text-base label-text'>Confirm Password</span>
             </label>
             <input
+              value={user.confirmPassword}
+              onChange={(e) => setUser({ ...user, confirmPassword: e.target.value })}
+
               className='w-full input input-bordered h-10'
-              type="password" 
-              placeholder='Confirm Password'/>
+              type="password"
+              placeholder='Confirm Password' />
           </div>
           <div className='flex items-center my-4'>
             <div className='flex items-center'>
               <p>Male</p>
-              <input type="checkbox" defaultChecked className="checkbox mx-2" />
+              <input 
+                type="checkbox"
+                checked={user.gender == "male"}
+                onChange={() => handleCheckbox("male")}
+                defaultChecked
+                className="checkbox mx-2" />
             </div>
             <div className='flex items-center'>
               <p>Female</p>
-              <input type="checkbox" defaultChecked className="checkbox mx-2" />
+              <input 
+                checked={user.gender == "female"}
+                onChange={() => handleCheckbox("female")}
+                type="checkbox"
+                defaultChecked
+                className="checkbox mx-2" />
             </div>
           </div>
           <p className='text-center my-2'> Already have an account? <Link to="/login">
-            login 
+            login
           </Link></p>
           <div>
-            <button className='btn btn-block btn-sm mt-2 border border-slate-700'>Signup</button>
+            <button type='submit' className='btn btn-block btn-sm mt-2 border border-slate-700'>Signup</button>
           </div>
         </form>
       </div>
