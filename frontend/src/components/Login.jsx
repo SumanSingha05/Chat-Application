86ab1ecb6e313e682c5da9551757ab5ff5f6012b
SumanSingha05 <sumansingha8654@gmail.com>
2025-02-19 22:37:29 +0530

    redux setup done

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -2,13 +2,14 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import toast from 'react-hot-toast';
 import axios from 'axios';
-
+import {useDispatch} from "react-redux"
+import { setAuthUser } from '../redux/userSlice';
 const Login = () => {
     const [user, setUser] = useState({
       username: "",
       password: "",
     });
-
+    const dispatch = useDispatch();
     const navigate = useNavigate();
 
     const onSubmitHandler = async (e) => {
@@ -22,8 +23,7 @@ const Login = () => {
         });
         
           navigate("/")
-         console.log(res);
-        
+         dispatch(setAuthUser(res.data))
       }catch(error){
         toast.error(error.response.data.message);
         console.log(error);
