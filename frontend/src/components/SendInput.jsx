0a95c80f0090d0b1b02d32dae1521be14745434e
SumanSingha05 <sumansingha8654@gmail.com>
2025-02-21 21:00:14 +0530

    minor changes done

--- frontend/src/components/SendInput.jsx
+++ frontend/src/components/SendInput.jsx
@@ -1,19 +1,39 @@
-import React from 'react';
-import {IoSend} from "react-icons/io5";
-const SendInput = () => {
-  return (
-    <form className='px-4 my-3'>
-        <div className='w-full relative'>
-            <input
-            type='text'
-            placeholder='Send a message...'
-            className='border text-sm rounded-lg block w-full p-3 border-zinc-500 bg-gray-700 text-white'
+import axios from 'axios';
+import React, { useState } from 'react';
+import { IoSend } from "react-icons/io5";
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
-            />
-            <button className='absolute flex inset-y-0 end-0 items-center pr-4'>
-                <IoSend/>
-            </button>
-        </div>
+const SendInput = () => {
+  const [message, setMessage] = useState("");
+  const dispatch = useDispatch();
+  const { selectedUser } = useSelector (store => store.user);
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(`http://localhost:8080/api/v1/message/send/${selectedUser?._id}`);
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+    alert(message);
+  }
+  return (
+    <form onSubmit={onSubmitHandler} className='px-4 my-3'>
+      <div className='w-full relative'>
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          type='text'
+          placeholder='Send a message...'
+          className='border text-sm rounded-lg block w-full p-3 border-zinc-500 bg-gray-700 text-white'
+
+        />
+        <button type='submit' className='absolute flex inset-y-0 end-0 items-center pr-4'>
+          <IoSend />
+        </button>
+      </div>
     </form>
   )
 }
