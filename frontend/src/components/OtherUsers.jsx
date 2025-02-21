8e8ae49d99b855445cc99983adc1a094dfa57af5
SumanSingha05 <sumansingha8654@gmail.com>
2025-02-21 14:48:35 +0530

    other user fetching from database done

--- frontend/src/components/OtherUsers.jsx
+++ frontend/src/components/OtherUsers.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import OtherUser from './OtherUser'
 import useGetOtherUsers from '../hooks/useGetOtherUsers';
-const OtherUsers = () => {
+import { useSelector } from 'react-redux';
 
+const OtherUsers = () => {
+    // my custom hook
   useGetOtherUsers();
+  const {otherUsers} = useSelector(store => store.user);
+    if(!otherUsers) return; // early return in react
 
   return (
     <div className='overflow-auto'>
-      <OtherUser/>
-      <OtherUser/>
-      <OtherUser/>
-      <OtherUser/>
-      <OtherUser/>
-      <OtherUser/>
-      <OtherUser/>
-      <OtherUser/>
-      <OtherUser/>
+    {
+      otherUsers?.map((user)=> {
+        return (
+          <OtherUser key={user._id} user={user} />
+        )
+      })
+    }
 
     </div>
   )
