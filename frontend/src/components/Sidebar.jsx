3b1823c4fe13702cb711a9a44743f668f5c0002c
SumanSingha05 <sumansingha8654@gmail.com>
2025-02-22 20:34:37 +0530

    search user feature added

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BiSearchAlt2 } from "react-icons/bi";
-import OtherUsers from './OtherUsers';
 import axios from 'axios';
 import toast from 'react-hot-toast'
 import {useNavigate} from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { setOtherUsers } from '../redux/userSlice';
+import OtherUsers from './OtherUsers';
 
 const Sidebar = () => {
+  
+  const [search, setSearch] = useState("");
+  const {otherUsers} = useSelector(store=>store.user);
+  const dispatch = useDispatch();
+
   const navigate = useNavigate();
+
   const logoutHandler = async () => {
     try {
-
       const res  = await axios.get(`http://localhost:8080/api/v1/user/logout`);
       navigate("/login");
       toast.success(res.data.message);
@@ -17,10 +24,24 @@ const Sidebar = () => {
       console.log(error)
     }
   }
+
+  const searchSubmitHandler = (e) => {
+    e.preventDefault(); // this is added so that the page doesn't refresh by itself.
+    const conversationUser = otherUsers?.find((user) => user.fullName.toLowerCase().includes(search.toLowerCase()));          
+    if(conversationUser){
+      dispatch(setOtherUsers([conversationUser]));
+    }
+    else {
+      toast.error("User not found!");
+    }
+  }
+
   return (
     <div className='border-r border-slate-500 p-4 flex flex-col'>
-      <form action="" className='flex items-center gap-2'>
-        <input 
+      <form onSubmit={searchSubmitHandler} action="" className='flex items-center gap-2'>
+        <input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
           className='input input-bordered rounded-md' type="text"
           placeholder='Search...'
         />
