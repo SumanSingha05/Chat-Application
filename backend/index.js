cbac22bda7b1ddf3c2e7d1085afcddcaea4a4229
SumanSingha05 <sumansingha8654@gmail.com>
2025-03-13 12:03:10 +0530

    bug

--- backend/index.js
+++ backend/index.js
@@ -5,31 +5,30 @@ import userRoute from "./routes/userRoute.js"
 import cookieParser from "cookie-parser"
 import messageRoute from "./routes/messageRoute.js"
 import cors from "cors";
-// import { server } from "./socket/socket.js";
+import { server } from "./socket/socket.js";
 
 
 dotenv.config({});
 
-const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 //middleware
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
-app.use(cookieParser());
+server.use(express.urlencoded({extended:true}));
+server.use(express.json());
+server.use(cookieParser());
 const corsOption={
     origin:'http://localhost:5173',
     credentials:true
 };
-app.use(cors(corsOption));
+server.use(cors(corsOption));
 
 // routes
-app.use("/api/v1/user",userRoute);
-app.use("/api/v1/message",messageRoute);
+server.use("/api/v1/user",userRoute);
+server.use("/api/v1/message",messageRoute);
 // http://localhost:8080/api/v1/user/register;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     connectDB();
     console.log(`Server listen at Port ${PORT}`);
 })
